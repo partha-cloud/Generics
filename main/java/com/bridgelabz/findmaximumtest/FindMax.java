418decae53c3b9133b16c10d994f90ac5a99b7a2
partha-cloud <partha11dey@gmail.com>
2021-04-13 16:10:49 +0530

    added UC2

--- main/java/com/bridgelabz/findmaximumtest/FindMax.java
+++ main/java/com/bridgelabz/findmaximumtest/FindMax.java
@@ -6,7 +6,7 @@ public class FindMax {
         System.out.println("***** Welcome to arithmetic number computation ****");
     }
 
-    public <T extends Integer> Integer findMaxValue(T firstNumber, T secondNumber, T thirdNumber)
+    public <T extends Integer> Integer findMaxValueInteger(T firstNumber, T secondNumber, T thirdNumber)
     {
         Integer max = firstNumber;
         if (secondNumber.compareTo(max) > 0) {
@@ -18,10 +18,24 @@ public class FindMax {
         printMax(max);
         return max;
     }
-
+    public <T extends Float> Float findMaxValueFloat(T firstNumber, T secondNumber, T thirdNumber)
+    {
+        Float max = firstNumber;
+        if (secondNumber.compareTo(max) > 0) {
+            max = secondNumber;
+        }
+        if (thirdNumber.compareTo(max) > 0) {
+            max = thirdNumber;
+        }
+        printMax(max);
+        return max;
+    }
     public <T extends Integer> void printMax(T max)
     {
         System.out.println("Maximum number is  = " + max);
     }
+    public <T extends  Float> void printMax(T max)
+    {
+        System.out.println("Maximum from float = " + max);
+    }
 }
-
